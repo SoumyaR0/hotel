fce5beecd17c2041e70c2a6976d1614032648a14
SoumyaR0 <soumyaranjanpatra730@gmail.com>
2024-12-16 00:30:43 +0530

    Initial version, be relaxed.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const db= require('./db');
+
+app.get('/',(req,res)=>{
+    res.send("Menu is coming");
+});
+app.get('/idli',(req,res)=>{
+    res.send("may i serve you idli sir..");
+});
+
+app.listen(3000,()=>{
+    console.log("app is listening to port 3000")
+});
